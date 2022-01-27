b18d6693db5291472a823df327846f64b3117696
Abhishek Pawar <42414536+kuriocity@users.noreply.github.com>
2022-01-27 19:19:30 +0530

    Time: 21 ms (8.33%), Space: 44.2 MB (7.02%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.java
+++ 97-interleaving-string/97-interleaving-string.java
@@ -1,14 +1,17 @@
 class Solution {
     HashMap<String,Boolean> map=new HashMap<>();
     public boolean isInterleave(String s1, String s2, String s3) {
-        
+        if (s1.length() + s2.length() != s3.length()) {
+            return false;
+        }
         return solve(s1,0,s2,0,s3,0);
     }
     
     boolean solve(String s1,int i, String s2,int j, String s3,int k){
         if(k==s3.length() && i==s1.length() && j==s2.length())
             return true;
-       
+        if(k==s3.length())
+            return false;
         
         boolean ans;
         if(map.containsKey(i+" "+j+" "+k)){
@@ -17,13 +20,13 @@ class Solution {
         
         if(i<s1.length() && j<s2.length() && s1.charAt(i)==s2.charAt(j) && 
                 k<s3.length() && s2.charAt(j)==s3.charAt(k))
-            ans= solve(s1,i+1,s2,j,s3,k+1) || solve(s1,i,s2,j+1,s3,k+1);
+            ans = solve(s1,i+1,s2,j,s3,k+1) || solve(s1,i,s2,j+1,s3,k+1);
         else if(i<s1.length() && k<s3.length() && s1.charAt(i)==s3.charAt(k))
-            ans= solve(s1,i+1,s2,j,s3,k+1);
+            ans = solve(s1,i+1,s2,j,s3,k+1);
         else if(j<s2.length() && k<s3.length() && s2.charAt(j)==s3.charAt(k))
-            ans= solve(s1,i,s2,j+1,s3,k+1);
+            ans = solve(s1,i,s2,j+1,s3,k+1);
         else
-            ans= false;
+            ans = false;
         
         map.put(i+" "+j+" "+k,ans);
         
